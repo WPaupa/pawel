551aae5bedcdc63635b19d05bdd02672a225c7a0
WPaupa <wojciechpaupa@gmail.com>
2023-05-13 00:03:18 +0200

    Monady ciąg dalszy

--- good/Prelude.ml
+++ good/Prelude.ml
@@ -1,6 +1,6 @@
-op right 0 $  = [$];;
-op right 2 || = [||];;
-op right 3 && = [&&];;
+op right 0 $  = {$};;
+op right 2 || = {||};;
+op right 3 && = {&&};;
 op right 4 == = [==];;
 op right 4 /= = [/=];;
 op right 4 <  = [<];;
@@ -12,20 +12,27 @@ op left  6 -  = [-];;
 op left  7 /  = [/];;
 op left  7 *  = [*];;
 op left  7 mod = modulo;;
-op right 8 **  = [**];;
+op right 8 ** = [**];;
+op right 9 o  = {o};;
+op left  9 !! = {!!};; 
 
 op right 5 ,  = Cons;;
 op right 5 :: = Cons;;
-op right 5 ++ = [++];;
+op right 5 ++ = {++};;
 op right 5 has = elem;;
 
 let id x = x;;
-let [$] f x = f x;;
+let {$} f x = f x;;
+let {o} f g x = f (g x);;
+let const x y = x;;
 let flip f x y = f y x;;
+let undefined x = undefined x;; 
+-- zaaplikowanie undefined do dowolnego wyrazu zawiesza program,
+-- ale samo undefined nie jest jeszcze wyliczone
 
 let int (x:Int) = x;; -- funkcja ukonkretniająca 
 
--- Redefiniujemy operatory bazowe w przeładowany sposób
+-- Redefiniujemy operatory bazowe w przeładowany sposób.
 let [+]  (a:Int) (b:Int) = {+} a b;;
 let [-]  (a:Int) (b:Int) = {-} a b;;
 let [*]  (a:Int) (b:Int) = {*} a b;;
@@ -41,6 +48,8 @@ let [**] (a:Int) (b:Int) = b ({*} a) 1;;
 let [/=] a b = {-} 1 ({==} a b);;
 
 -- Teraz możemy je zdefiniować dla innych typów
+-- Konwencja: operatory przeładowane dla funkcji
+-- oznaczamy w nawiasach kwadratowych, a bazowe - w klamrowych
 let [+]  (a:a->b) (b:a->b) x = {+} (a x) (b x);;
 let [-]  (a:a->b) (b:a->b) x = {-} (a x) (b x);;
 let [*]  (a:a->b) (b:a->b) x = {*} (a x) (b x);;
@@ -86,10 +95,14 @@ let [/=] a (b:a->b) x = {-} 1 ({==} a (b x));;
 -- ale bez szybkiego ukonkretnienia często będą mnożyć się dostępne przeładowania
 let modulo a b = a - (b * (a / b));;
 
+-- Warto mieć też modulo tylko dla intów
+let {mod} a b = {-} a ({*} b ({/} a b));;
+
 -- Operatory logiczne, dla intów troszczymy się, żeby nie wybuchło 
-let ifzero a = if a then 1 else 0;; 
-let [&&] (a:Int) (b:Int) = int $ (ifzero a) * (ifzero b);;
-let [||] (a:Int) (b:Int) = ifzero $ (ifzero a) + (ifzero b);;
+let ifnzero a = if a then 1 else 0;; 
+let ifzero a = if a then 0 else 1;;
+let {&&} (a:Int) (b:Int) = int $ (ifnzero a) * (ifnzero b);;
+let {||} (a:Int) (b:Int) = ifnzero $ (ifnzero a) + (ifnzero b);;
 
 -- Pary
 type Pair of a b = Pair a b;;
@@ -137,15 +150,26 @@ let foldLeft  f acc list = match list with
 let map f list = match list with 
     Empty => Empty 
     | Cons a b => Cons (f a) (map f b);;
+let filter pred list = match list with
+    Empty => Empty
+    | Cons a b => if pred a then Cons a (filter pred b) else filter pred b;;
 
--- Listy można matchować także operatorami
-let [++] l m = match l with Empty => m | h, t => h, (t ++ m);;
+-- Listy można matchować także operatorem ,
+let {++} l m = match l with Empty => m | h, t => h, (t ++ m);;
+let flatten list = match list with
+    Empty => Empty
+    | h,t => h ++ flatten t;;
+let {!!} list n = match list with 
+    h, t => if ifzero n then h else {!!} t ({-} n 1);;
 
 -- Pełen (chociaż trochę dziwny) lukier syntaktyczny dla list można zdefiniować w języku
 let [ x = x;;
 let ] = Empty;;
 -- Dzięki temu można na przykład tworzyć listy typu [1,2,3, ] 
 -- (nie można zapominać o przecinku i spacji na końcu, żeby ,] nie zostało potraktowane jako osobny operator)
+-- Sprawia to też, że listy nie mają odgórnej precedencji, tylko parsują się zgodnie z precedencją operatorów.
+-- Z tego powodu na przykład [1, 2, 3, ] !! 2 sparsuje się jako 1, 2, 3, (Empty !! 2) o poprawnym typie i błędzie matcha
+-- Należy więc w takich sytuacjach używać nawiasów.
 
 -- Ta definicja stworzy kopie procedury elem dla każdego typu elementów listy, którego
 -- przeładowanie porównania zwraca inta (coś, co może być pod ifem). Dlatego musi być
--- good/monads.ml
+++ good/monads.ml
@@ -7,11 +7,43 @@ op left 1 >> = {>>};;
 -- czyli dopóki obliczenia są abstrakcyjne, to możemy obserwować,
 -- jak wyglądałby wynik w zależności od tego, jakiej monady używamy.
 -- Na przykład return 5 dostaje typ 
+-- Forall a10. overload(State(a10,Int),
+--                   Reader(a10,Int),
+--                   Maybe(Int),
+--                   Either(a10,Int),
+--                   List(Int))
+
+let {>>=} (a:List of a) (f:b -> List of b) = flatten (map f a);;
+let return (a:a) = [a, ];;
 
 type Maybe of a = Just a | Nothing;;
-type Either of a b = Left a | Right b;;
-type Reader of env result = Reader (env -> result);;
+let {>>=} (a:Maybe of a) (f:a->Maybe of b) = match a with
+    Nothing => Nothing
+    | Just a => f a;;
+let return (a:a) = Just a;;
 
+type Either of a b = Left a | Right b;;
+let {>>=} (a:Either of a b) (f:b -> Either of a c) = match a with
+    Left l => Left l
+    | Right r => f r;;
+let return (a:a) = Right a;;
+
+type State of state result = State (state -> Pair of result state);;
+let runState a b = match a with
+    State f => f b;;
+let {>>=} (a:State of a b) (f:b -> State of a b) = match a with
+    State f' => State (λs.
+        let pair = f' s in match pair with
+            a ,, s' => runState (f a) s'
+    );;
+let return (a:a) = State (λs . a,, s);;
+let get = State (λs. s,, s);;
+let put s = State (λ_ . (Nothing,, s));;
+let modify f = get >>= (λx.put (f x));;
+let execState f = snd o runState f;; -- tutaj o jest złożeniem, funkcja z Prelude.ml
+let evalState f = fst o runState f;;
+
+type Reader of env result = Reader (env -> result);;
 let reader = Reader;;
 let runReader a b = match a with
     Reader f => f b;;
@@ -21,16 +53,6 @@ let return (a:a) = Reader (λ_ . a);;
 let ask = Reader id;;
 let local f a = match a with
     Reader f' => Reader $ λ env. f' (f env);;
-
-let {>>=} (a:Maybe of a) (f:a->Maybe of b) = match a with
-    Nothing => Nothing
-    | Just a => f a;;
-let return (a:a) = Just a;;
-
-let {>>=} (a:Either of a b) (f:b -> Either of a c) = match a with
-    Left l => Left l
-    | Right r => f r;;
-let return (a:a) = Right a;;
 
 -- Mamy zdefiniowane kilka monad, więc możemy podefiniować
 -- operacje monadyczne na nich.
@@ -43,3 +65,8 @@ let foldM (f : b -> a -> m) (acc : b) (l : List of a) = match l with
     | h,t => (f acc h) >>= (λfah. foldM f fah t);;
 
 let {>>} a b = {>>=} a (λ_  . b);;
+let fmap f m = m >>= (return o f);;
+let pure = return;;
+
+op left 4 <*> = {<*>};;
+let {<*>} mfab ma = mfab >>= (λfab. ma >>= (return o fab));;
